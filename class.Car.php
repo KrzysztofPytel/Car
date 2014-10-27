0147bc6cd371b7b523d20086bf5983c6f7458476
KrzysztofPytel <krzysztof_pytel@vp.pl>
2014-10-27 14:49:36 +0100

    Update class.Car.php

--- class.Car.php
+++ class.Car.php
@@ -5,16 +5,15 @@ require('class.Engine.php');
 
 class Car implements RaceCarInterface {
 
+    private $model;
     private $vMax;
     private $acc;
-    private $model;
     private $hp;
-    
+
     public function __construct($model, $hp) {
         $this->model = $model;
         $engine = new Engine();
-        $engine->setHP($hp);
-        $this->hp = $engine->getHP();
+        $this->hp = $engine->getHP($hp);
     }
 
     public function getVMax() {
